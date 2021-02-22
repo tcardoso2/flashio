8cb359ec4f197dc32a67d8450c601b3f5d34d3da
pi_droneci <tcardoso@gmail.com>
2021-02-23 00:03:21 +0800

    Added redis write to receive

--- receive.js
+++ receive.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
 var amqp = require('amqplib/callback_api');
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+//client.set("key2", "value2", redis.print);
+client.get("key2", redis.print);
+
 
 amqp.connect('amqp://user:password@localhost', function(error0, connection) {
   if (error0) {
@@ -17,7 +27,10 @@ amqp.connect('amqp://user:password@localhost', function(error0, connection) {
     });
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
     channel.consume(queue, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
+      let _msg = msg.content.toString();
+      console.log(" [x] Received %s", _msg);
+      //Redis test
+      client.set(_msg, `value_${_msg}`, redis.print);
     }, {
       noAck: true
     });
